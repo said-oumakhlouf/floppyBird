9560d4323d0f2cb688a069e31394889c70d124a2
said-oumakhlouf <oumakhlouf.said@gmail.com>
2021-05-17 23:18:48 +0200

    Change axe jump and flight Floppy

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ let gamePlaying = false;
 const gravity = .5;
 const speed = 6.2;
 const size = [51, 36];
-const jump = 11.5;
+const jump = -11.5;
 const cTenth = (canvas.width / 10);
 
 let index = 0,
     bestScore = 0,
     currentScore = 0,
     pipes = [],
-    fligth,
+    flight,
     flyHeight;
 
 const render = () => {
@@ -23,12 +23,23 @@ const render = () => {
 
     // Background
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed / 2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height);
-
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed / 2)) % canvas.width), 0, canvas.width, canvas.height);
 
-    ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width / 2) - size[0] / 2), flyHeight, ...size);
-    flyHeight = (canvas.height / 2) -(size[1] / 2);
+    if(gamePlaying) {
+        ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, cTenth, flyHeight, ...size);
+        flight += gravity;
+        flyHeight = Math.min(flyHeight + flight, canvas.height - size[1]);
+    } else {
+        ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width / 2) - size[0] / 2), flyHeight, ...size);
+        flyHeight = (canvas.height / 2) - (size[1] / 2);
+    
+        ctx.fillText(`Meilleur score : ${bestScore}`, 55, 245);
+        ctx.fillText('Cliquez pour jouer', 48, 535);
+        ctx.font = "bold 30px courier";
+    }
 
     window.requestAnimationFrame(render);
 }
 img.onload = render;
+document.addEventListener('click', () => gamePlaying = true);
+window.onclick = () => flight = jump;
